28b7df480164d3a0b0a4a9aa132ad1e078f6afd0
Демченко Сергей <Demniks@ukr.net>
2023-01-17 18:07:22 +0200

    update

--- js/task-09.js
+++ js/task-09.js
@@ -9,3 +9,28 @@ function getRandomHexColor() {
   ).toString(16)}`;
   spanColor.textContent = document.body.style.backgroundColor;
 }
+
+// const changeButton = document.querySelector(".change-color");
+// const spanColor = document.querySelector(".color");
+// const body = document.querySelector("body");
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// changeButton.addEventListener("click", handleClick);
+// function handleClick() {
+//   body.style.backgroundColor = getRandomHexColor();
+//   spanColor.textContent = body.style.backgroundColor;
+// }
+
+// const refs = {
+//   changeCoolorBtn: document.querySelector(".change-color"),
+//   body: document.querySelector("body"),
+//   colorName: document.querySelector(".color")
+// };
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// refs.changeCoolorBtn.addEventListener("click", () => {
+//   refs.body.style.backgroundColor = getRandomHexColor();
+//   refs.colorName.textContent = refs.body.style.backgroundColor;
+// })
